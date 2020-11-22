7e1db4c235d037c9d3510383229814bc47d5d9c4
diakitelamine <42340818+diakitelamine@users.noreply.github.com>
2020-11-22 17:01:48 +0100

    Modification de formulaire d'inscription

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,59 @@
-const { hash } = require('bcrypt');
-const express = require('express');
-const { createUser } = require('../queries/user.queries');
+const User = require('../models/auth.model');
+const expressJwt = require('express-jwt');
 
-exports.userNew = (req, res, next) => {
-    res.render('signup', {error:null});
-  }
+exports.readController = (req, res) => {
+    const userId = req.params.id;
+    User.findById(userId).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json(user);
+    });
+};
 
+exports.updateController = (req, res) => {
 
-  exports.userCreate = async (req, res, next) => {
-    try {
-        const body= req.body;
-        const user= await  createUser(body);
-        req.login(user, (err)=>{
-          if(err){ next(err) }
-          res.redirect('/');
+    console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
+    const { name, password } = req.body;
 
-        })
-    } catch (e) {
-        res.render('signup', {error: e.message});
-    }
-  }
+    User.findOne({ _id: req.user._id }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        if (!name) {
+            return res.status(400).json({
+                error: 'Name is required'
+            });
+        } else {
+            user.name = name;
+        }
+
+        if (password) {
+            if (password.length < 6) {
+                return res.status(400).json({
+                    error: 'Password should be min 6 characters long'
+                });
+            } else {
+                user.password = password;
+            }
+        }
+
+        user.save((err, updatedUser) => {
+            if (err) {
+                console.log('USER UPDATE ERROR', err);
+                return res.status(400).json({
+                    error: 'User update failed'
+                });
+            }
+            updatedUser.hashed_password = undefined;
+            updatedUser.salt = undefined;
+            res.json(updatedUser);
+        });
+    });
+};
--- index.js
+++ index.js
@@ -1,20 +1,66 @@
-const path=require('path');
-const express = require('express');
-require('./database');
-const router =require('./routes');
+const express = require('express')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const app = express();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    res.header("Content-Type", "application/json");
+    next();
+});
 
-exports.app=app;
+global.bdd = require('./models/auth.model');
+// Config dotev
+require('dotenv').config({
+    path: './config/config.env'
+})
 
-require('./config/session.config');
-require('./config/passport.config');
+
+// Connect to database
+connectDB();
+
+// body parser
+// Emplacement du dossier public dans express
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(cors());
+// Route par defautl - Importante
+app.get('*', (req, res) => {
+    // Verif user
+    res.sendFile(`index.html`, { root: www });
+});
+
+// Load routes
+const authRouter = require('./routes/auth.route')
+const userRouter = require('./routes/user.route')
+
+// Dev Logginf Middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }))
+    app.use(morgan('dev'))
+}
 
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(router);
+// Use Routes
+app.use('/api', authRouter)
+app.use('/api', userRouter)
 
-app.listen(3000);
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        msg: "Page not founded"
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
