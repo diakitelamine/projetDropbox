dadcb47f62a641d48afaf35c879e9547dfcd3592
diakitelamine <42340818+diakitelamine@users.noreply.github.com>
2020-12-21 14:36:11 +0100

    La route pour la suppression d'un fichier

--- server/routes/file.js
+++ server/routes/file.js
@@ -78,4 +78,16 @@ Router.get('/download/:id', async (req, res) => {
   }
 });
 
+Router.delete('/delete/:id', async (req, res) => {
+  try {
+    const file = await File.findById(req.params.id);
+    res.set({
+      'Content-Type': file.file_mimetype
+    });
+    res.sendFile(path.join(__dirname, '..', file.file_path));
+  } catch (error) {
+    res.status(400).send('Error while downloading file. Try again later.');
+  }
+});
+
 module.exports = Router;
