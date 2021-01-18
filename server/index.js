0091d62a3ba736a534539e2ab15e64fadf6addaa
diakitelamine <42340818+diakitelamine@users.noreply.github.com>
2021-01-18 09:46:23 +0100

    edit

--- server/index.js
+++ server/index.js
@@ -31,15 +31,14 @@ app.use(cookieParser());
 const userRouter = require("./routes/user");
 
 app.use("/api/users", userRouter);
+app.use(fileRoute); 
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, '..', 'build')));
 
   app.use(express.static(path.join(__dirname, '..', 'build')));
-  app.use(fileRoute);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
